84a51e30d524da62fab615aa35b829a3cef9e846
huzaifashuja <huzaifashuja471@gmail.com>
2020-06-28 13:58:43 +0500

    google map work

---
+++ README.md
@@ -0,0 +1,64 @@
+# example/dio.dart
+
+```dart
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:google_map_location_picker/generated/i18n.dart'
+    as location_picker;
+import 'package:google_map_location_picker/google_map_location_picker.dart';
+import 'package:google_map_location_picker_example/keys.dart';
+
+import 'generated/i18n.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  LocationResult _pickedLocation;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'location picker',
+      localizationsDelegates: const [
+        location_picker.S.delegate,
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const <Locale>[
+        Locale('en', ''),
+        Locale('ar', ''),
+      ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('location picker'),
+        ),
+        body: Builder(builder: (context) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () async {
+                    LocationResult result = await showLocationPicker(context, apiKey);
+                    print("result = $result");
+                    setState(() => _pickedLocation = result);
+                  },
+                  child: Text('Pick location'),
+                ),
+                Text(_pickedLocation.toString()),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+```
---
+++ lib/main.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:google_map_location_picker/generated/i18n.dart'
+    as location_picker;
+import 'package:google_map_location_picker/google_map_location_picker.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import 'generated/i18n.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  LocationResult _pickedLocation;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+//      theme: ThemeData.dark(),
+      title: 'location picker',
+      localizationsDelegates: const [
+        location_picker.S.delegate,
+        S.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const <Locale>[
+        Locale('en', ''),
+        Locale('ar', ''),
+        Locale('pt', ''),
+        Locale('tr', ''),
+      ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('location picker'),
+        ),
+        body: Builder(builder: (context) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () async {
+                    LocationResult result = await showLocationPicker(
+                      context,
+                      "AIzaSyAdynPwusxRGr6CjrUaNhpns5MFWZ3a7qM",
+                      initialCenter: LatLng(31.1975844, 29.9598339),
+//                      automaticallyAnimateToCurrentLocation: true,
+//                      mapStylePath: 'assets/mapStyle.json',
+                      myLocationButtonEnabled: true,
+                      layersButtonEnabled: true,
+//                      resultCardAlignment: Alignment.bottomCenter,
+                    );
+                    print("result = $result");
+                    setState(() => _pickedLocation = result);
+                  },
+                  child: Text('Pick location'),
+                ),
+                Text(_pickedLocation.toString()),
+              ],
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
